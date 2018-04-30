232b8b3aced07fecedf66dcbb1ca277a019406cd
Annie553 <huanghuimin100@gmail.com>
2018-04-30 09:38:09 +0930

    v2 updated all

--- js/school.js
+++ js/school.js
@@ -1,1 +1,21 @@
-var body= document.body;
+/*put the submenu's special class name as arguments, if find a match then add the show class
+to the submenu*/
+var arrays = new Array(".about",".news_events",".fundraising",".parents_portal",".contact");
+//store opended submenu
+var openedmenu;
+function dropDown(tagId) {
+  if(openedmenu!=null){
+    //hide the previous opened submenu
+    openedmenu.classList.remove("show");
+  }
+  for (var i = 0; i < arrays.length; i++) {
+    console.log(tagId);
+    if (arrays[i] == tagId) {
+      console.log(arrays[i]);
+      document.querySelector(arrays[i]).classList.toggle("show");
+      //record the opened submenu
+      openedmenu=document.querySelector(arrays[i]);
+    }
+  }
+
+}
